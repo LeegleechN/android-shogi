ae812ed710e04210025ba42c082d08f107fda63b
LeegleechN <LeegleechN@22cdadd4-b578-0a24-35cd-2f8311ab5adc>
2010-06-06 23:44:19 +0000

    added eclipse project with JNI sources

---
+++ trunk/Shogi/src/com/stelluxstudios/Shogi/Engine.java
@@ -0,0 +1,17 @@
+package com.stelluxstudios.Shogi;
+
+public class Engine
+{
+	public Engine()
+	{
+		
+	}
+	
+	public native void newGame();
+	
+	public native void applyMove(byte[] move);
+	public native void makeMove();
+	
+	public native byte[] getBoardString();
+	
+}
