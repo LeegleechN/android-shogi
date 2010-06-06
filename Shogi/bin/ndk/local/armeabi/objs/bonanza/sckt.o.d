c768b3245b0929d2d2fc89f90307f123e7c772d4
LeegleechN <LeegleechN@22cdadd4-b578-0a24-35cd-2f8311ab5adc>
2010-06-06 23:44:19 +0000

    added eclipse project with JNI sources

---
+++ Shogi/bin/ndk/local/armeabi/objs/bonanza/sckt.o.d
@@ -0,0 +1,182 @@
+/media/Data/workspace/android-shogi/Shogi/bin/ndk/local/armeabi/objs/bonanza/sckt.o: \
+ /media/Data/workspace/android-shogi/Shogi/jni/sckt.c \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/arpa/inet.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/netinet/in.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/endian.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/socket.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/socket.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/sockios.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/sockios.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/uio.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/in.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/byteorder.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/in6.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/netinet/in6.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/socket.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/netdb.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /media/Data/workspace/android-shogi/Shogi/jni/shogi.h \
+ /media/Data/workspace/android-shogi/Shogi/jni/param.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/pthread.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/time.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sched.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/times.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/times.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/inttypes.h
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/arpa/inet.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/netinet/in.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/socket.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/socket.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/sockios.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/sockios.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/uio.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/in.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/byteorder.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/in6.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/netinet/in6.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/socket.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/netdb.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/media/Data/workspace/android-shogi/Shogi/jni/shogi.h:
+
+/media/Data/workspace/android-shogi/Shogi/jni/param.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/pthread.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/time.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sched.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/times.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/times.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/inttypes.h:
---
+++ Shogi/bin/ndk/local/armeabi/objs/bonanza/searchr.o.d
@@ -0,0 +1,128 @@
+/media/Data/workspace/android-shogi/Shogi/bin/ndk/local/armeabi/objs/bonanza/searchr.o: \
+ /media/Data/workspace/android-shogi/Shogi/jni/searchr.c \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/assert.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/memory.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /media/Data/workspace/android-shogi/Shogi/jni/shogi.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /media/Data/workspace/android-shogi/Shogi/jni/param.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/pthread.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/time.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sched.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/times.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/times.h \
+ /home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/inttypes.h
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/assert.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/media/Data/workspace/android-shogi/Shogi/jni/shogi.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/media/Data/workspace/android-shogi/Shogi/jni/param.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/pthread.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/time.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sched.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/sys/times.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/linux/times.h:
+
+/home/nisarg/Desktop/android-ndk-r4/build/platforms/android-3/arch-arm/usr/include/inttypes.h:
