601a381a4bd2b293ee850007af549e913ea275b5
LeegleechN <LeegleechN@22cdadd4-b578-0a24-35cd-2f8311ab5adc>
2010-07-06 23:29:47 +0000

    restored 'make move' button for demo purposes

--- trunk/Shogi/src/com/stelluxstudios/Shogi/Board.java
+++ trunk/Shogi/src/com/stelluxstudios/Shogi/Board.java
@@ -26,6 +26,38 @@ public class Board {
 "P8 * +KA *  *  *  *  * +HI * \n"+
 "P9+KY+KE+GI+KI+OU+KI+GI+KE+KY\n";
 	
+	//these should be in Piece, but I couldn't get that placement to work with the Piece constructors
+	public final static int 
+	FORWARD_ONE =		 0x00000001,
+	BACK_ONE = 			 0x00000002,
+	LEFT_ONE = 			 0x00000004,
+	RIGHT_ONE =			 0x00000008,
+	
+	DIAG_FORWARD_LEFT_ONE =  0x00000010,
+	DIAG_FORWARD_RIGHT_ONE = 0x00000020,
+	DIAG_BACK_LEFT_ONE = 	 0x00000040,
+	DIAG_BACK_RIGHT_ONE = 	 0x00000080,
+	
+	FORWARD_SLIDE = 	 0x00000100,
+	BACK_SLIDE = 		 0x00000200,
+	LEFT_SLIDE = 		 0x00000400,
+	RIGHT_SLIDE = 		 0x00000800,
+	
+	DIAG_FORWARD_LEFT_SLIDE = 	 0x00001000,
+	DIAG_FORWARD_RIGHT_SLIDE = 	 0x00002000,
+	DIAG_BACK_LEFT_SLIDE = 		 0x00004000,
+	DIAG_BACK_RIGHT_SLIDE = 	 0x00008000,
+	
+	FORWARD_LEFT_KNIGHT= 0x00010000,
+	FORWARD_RIGHT_KNIGHT=0x00020000,
+	
+	GOLD_MOVEMENT =  FORWARD_ONE | DIAG_FORWARD_LEFT_ONE | DIAG_FORWARD_RIGHT_ONE | LEFT_ONE | RIGHT_ONE | BACK_ONE,
+	SILVER_MOVEMENT =  FORWARD_ONE | DIAG_FORWARD_LEFT_ONE | DIAG_FORWARD_RIGHT_ONE | DIAG_BACK_LEFT_ONE | DIAG_BACK_RIGHT_ONE,
+	KING_MOVEMENT = FORWARD_ONE | DIAG_FORWARD_LEFT_ONE | DIAG_FORWARD_RIGHT_ONE | LEFT_ONE | RIGHT_ONE | DIAG_BACK_LEFT_ONE | DIAG_BACK_RIGHT_ONE | BACK_ONE,
+	ROOK_MOVEMENT = FORWARD_SLIDE | BACK_SLIDE | LEFT_SLIDE | RIGHT_SLIDE,
+	BISHOP_MOVEMENT = DIAG_FORWARD_LEFT_ONE | DIAG_FORWARD_RIGHT_SLIDE | DIAG_BACK_LEFT_SLIDE | DIAG_BACK_RIGHT_SLIDE;
+	
+	
 	public static Board fromString(String in)
 	{
 		Board b = new Board();
@@ -154,45 +186,50 @@ public class Board {
 		return blackHand;
 	}
 	
+	
 	public static enum Piece{
-		WPawn           ("Pawn"            ,"","gfu"  ,"-FU"),
-		WPromotedPawn   ("Promoted Pawn"   ,"","gto"  ,"-TO"),
-		WLance          ("Lance"           ,"","gkyo" ,"-KY"),
-		WPromotedLance  ("Promoted Lance"  ,"","gnkyo","-NY"),
-		WKnight         ("Knight"          ,"","gkei" ,"-KE"),
-		WPromotedKnight ("Promoted Knight" ,"","gnkei","-NK"),
-		WSilverGeneral  ("Silver General"  ,"","ggin" ,"-GI"),
-		WPromotedSilver ("Promoted Silver" ,"","gngin","-NG"),
-		WGoldGeneral    ("Gold General"    ,"","gkin", "-KI"),
-		WBishop         ("Bishop"          ,"","gkaku","-KA"),
-		WDragonHorse    ("Dragon Horse"    ,"","guma" ,"-UM" ),
-		WRook           ("Rook"            ,"","ghi"  ,"-HI"),
-		WDragonKing     ("Dragon King"     ,"","gryu" ,"-RY" ),
-		WKing           ("King"            ,"","gou"  ,"-OU"),
-		BPawn           ("Pawn"            ,"","sfu"  ,"+FU"),
-		BPromotedPawn   ("Promoted Pawn"   ,"","sto"  ,"+TO"),
-		BLance          ("Lance"           ,"","skyo" ,"+KY"),
-		BPromotedLance  ("Promoted Lance"  ,"","snkyo","+NY"),
-		BKnight         ("Knight"          ,"","skei" ,"+KE"),
-		BPromotedKnight ("Promoted Knight" ,"","snkei","+NK"),
-		BSilverGeneral  ("Silver General"  ,"","sgin" ,"+GI"),
-		BPromotedSilver ("Promoted Silver" ,"","sngin","+NG"),
-		BGoldGeneral    ("Gold General"    ,"","skin", "+KI"),
-		BBishop         ("Bishop"          ,"","skaku","+KA"),
-		BDragonHorse    ("Dragon Horse"    ,"","suma" ,"+UM" ),
-		BRook           ("Rook"            ,"","shi"  ,"+HI"),
-		BDragonKing     ("Dragon King"     ,"","sryu" ,"+RY" ),
-		BKing           ("King"            ,"","sou"  ,"+OU"),
-		Empty           ("Empty","",""," * ");
+		WPawn           ("Pawn"            ,"","gfu"  ,"-FU", FORWARD_ONE),
+		WPromotedPawn   ("Promoted Pawn"   ,"","gto"  ,"-TO", GOLD_MOVEMENT),
+		WLance          ("Lance"           ,"","gkyo" ,"-KY", FORWARD_SLIDE),
+		WPromotedLance  ("Promoted Lance"  ,"","gnkyo","-NY", GOLD_MOVEMENT),
+		WKnight         ("Knight"          ,"","gkei" ,"-KE", FORWARD_LEFT_KNIGHT | FORWARD_RIGHT_KNIGHT),
+		WPromotedKnight ("Promoted Knight" ,"","gnkei","-NK", GOLD_MOVEMENT),
+		WSilverGeneral  ("Silver General"  ,"","ggin" ,"-GI", SILVER_MOVEMENT),
+		WPromotedSilver ("Promoted Silver" ,"","gngin","-NG", GOLD_MOVEMENT),
+		WGoldGeneral    ("Gold General"    ,"","gkin", "-KI", GOLD_MOVEMENT),
+		WBishop         ("Bishop"          ,"","gkaku","-KA", BISHOP_MOVEMENT),
+		WDragonHorse    ("Dragon Horse"    ,"","guma" ,"-UM", BISHOP_MOVEMENT | KING_MOVEMENT),
+		WRook           ("Rook"            ,"","ghi"  ,"-HI", ROOK_MOVEMENT),
+		WDragonKing     ("Dragon King"     ,"","gryu" ,"-RY", ROOK_MOVEMENT | KING_MOVEMENT),
+		WKing           ("King"            ,"","gou"  ,"-OU", KING_MOVEMENT),
+		BPawn           ("Pawn"            ,"","sfu"  ,"+FU", FORWARD_ONE),
+		BPromotedPawn   ("Promoted Pawn"   ,"","sto"  ,"+TO", GOLD_MOVEMENT),
+		BLance          ("Lance"           ,"","skyo" ,"+KY", FORWARD_SLIDE),
+		BPromotedLance  ("Promoted Lance"  ,"","snkyo","+NY", GOLD_MOVEMENT),
+		BKnight         ("Knight"          ,"","skei" ,"+KE", FORWARD_LEFT_KNIGHT | FORWARD_RIGHT_KNIGHT),
+		BPromotedKnight ("Promoted Knight" ,"","snkei","+NK", GOLD_MOVEMENT),
+		BSilverGeneral  ("Silver General"  ,"","sgin" ,"+GI", SILVER_MOVEMENT),
+		BPromotedSilver ("Promoted Silver" ,"","sngin","+NG", GOLD_MOVEMENT),
+		BGoldGeneral    ("Gold General"    ,"","skin", "+KI", GOLD_MOVEMENT),
+		BBishop         ("Bishop"          ,"","skaku","+KA", BISHOP_MOVEMENT),
+		BDragonHorse    ("Dragon Horse"    ,"","suma" ,"+UM", BISHOP_MOVEMENT | KING_MOVEMENT),
+		BRook           ("Rook"            ,"","shi"  ,"+HI", ROOK_MOVEMENT),
+		BDragonKing     ("Dragon King"     ,"","sryu" ,"+RY", ROOK_MOVEMENT | KING_MOVEMENT),
+		BKing           ("King"            ,"","sou"  ,"+OU", KING_MOVEMENT),
+		Empty           ("Empty","",""," * ",0);
 		
-		public String englishName, japName, shortJapName, japAbbr;
+
 		
-		Piece(String englishName, String japName,String shortJapName,String japAbbr)
+		public final String englishName, japName, shortJapName, japAbbr;
+		public final int movementCapabilities;
+		
+		Piece(String englishName, String japName,String shortJapName,String japAbbr, int movementCapabilities)
 		{
 			this.englishName = englishName;
 			this.japName = japName;
 			this.shortJapName = shortJapName;
 			this.japAbbr = japAbbr;
+			this.movementCapabilities = movementCapabilities;
 		};
 		
 		static Piece from3CharCode(String in)
