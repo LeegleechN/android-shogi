5d3ea881adc18a7944551249eeec186968687656
LeegleechN <LeegleechN@22cdadd4-b578-0a24-35cd-2f8311ab5adc>
2010-08-08 19:44:22 +0000

    Fixed merge problem

--- trunk/Shogi/src/com/stelluxstudios/Shogi/BoardView.java
+++ trunk/Shogi/src/com/stelluxstudios/Shogi/BoardView.java
@@ -147,7 +147,7 @@ public class BoardView extends ImageView {
 		
 		boolean drawingHintOverlays = (showMoveHints && currentUIState == UIState.Piece_Selected);
 		legalMovesForSelectedPiece = board.getValidMovesForPiece(selectedI, selectedJ);
-		
+
 		for (int i = 0 ; i < 9 ; i++)
 		{
 			for (int j = 0 ; j < 9 ; j++)
